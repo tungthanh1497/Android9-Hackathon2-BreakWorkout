b753eb296d1d30fd119aa375cb4a24611a11100c
ndam <valkyrie1127@gmail.com>
2017-06-29 18:15:46 +0700

    Added Alarm scence, circle shape, start button

---
+++ Android9-Hackathon2-BreakWorkout/app/src/main/java/techkids/com/android9_hackathon2_breakworkout/views/AlarmScene.java
@@ -0,0 +1,15 @@
+package techkids.com.android9_hackathon2_breakworkout.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import techkids.com.android9_hackathon2_breakworkout.R;
+
+public class AlarmScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_scene);
+    }
+}
